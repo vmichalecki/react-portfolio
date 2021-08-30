4908bb014a30e37c4d310f5e547b07cf9665bbcc
Valerie Michalecki <valerie227@gmail.com>
2021-08-30 12:40:06 -0500

    all pages styled

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,6 +13,8 @@ function About() {
             <div className={aboutClass}>
                 <p>
                     I'm a self-starting full stack developer from New York City with ambition and an accent who builds polished, functional products from front to back.
+
+                    I enjoy the outdoors as much as I enjoy designer makeup so when I'm not programming, you can find me outside. I'll be the one with lipstick and hiking boots on.
                 </p>
             </div>
         </div>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,73 +4,73 @@ import Quiz from '../../images/quiz-screen.png';
 import Weather from '../../images/weather-dashboard-screen.png';
 
 function Projects() {
-    let mainProject = 'container-first';
-    let mainImage = 'first-project';
+    let container = 'projects'
+    let project = 'container-first';
+    let image = 'first-project';
+
     return (
         <div>
+            <div className={container}>
+                <div className={project}>
+                    <a href="https://group-proj-1.github.io/beatwave/">
+                        <img
+                            className={image}
+                            src={Beatwave}
+                            alt={'Beatwave'}
+                        />
+                    </a>
+                    <div className="overlay">BEATWAVE</div>
+                </div>
 
-            <div className={mainProject}>
-                <a href="https://group-proj-1.github.io/beatwave/">
-                    <img
-                        className={mainImage}
-                        src={Beatwave}
-                        alt={'Beatwave'}
-                    />
-                </a>
-                <div className="overlay">BEATWAVE</div>
-            </div>
-
-            <div>
-
-                <div className="container">
+                <div className={project}>
                     <a href="https://vmichalecki.github.io/timed-quiz/">
                         <img
-                            className="projects"
+                            className={image}
                             src={Quiz}
                             alt={'the existential dread quiz'}
                         /></a>
                     <div className="overlay">The Existential Dread Quiz</div>
                 </div>
 
-                <div className="container">
+                <div className={project}>
                     <a href="https://vmichalecki.github.io/weather-dashboard/">
                         <img
-                            className="projects"
+                            className={image}
                             src={Weather}
                             alt="weather dashboard"
                         />
                     </a>
                     <div className="overlay">Weather Dashboard</div>
                 </div>
+            </div>
 
-                <div className="line-break"></div>
+            <div className="line-break"></div>
 
-                <div className="repo-container">
-                    <h2>Repositories</h2>
-                    <ul className="repo-ul">
-                        <li>
-                            <a className="repo-li" href="https://github.com/group-proj-1/beatwave"
-                            >BEATWAVE</a
-                            >
-                        </li>
-                        <li>
-                            <a className="repo-li" href="https://github.com/vmichalecki/timed-quiz"
-                            >The Existential Dread Quiz</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                className="repo-li"
-                                href="https://github.com/vmichalecki/weather-dashboard"
-                            >Weather Dashboard</a
-                            >
-                        </li>
-                    </ul>
-                </div>
+            <div className="repo-container">
+                <h2>Repositories</h2>
+                <p className="repo-ul">
 
+                    <a className="repo-li" href="https://github.com/group-proj-1/beatwave"
+                    >BEATWAVE</a
+                    >
+
+
+                    <a className="repo-li" href="https://github.com/vmichalecki/timed-quiz"
+                    >The Existential Dread Quiz</a
+                    >
+
+
+                    <a
+                        className="repo-li"
+                        href="https://github.com/vmichalecki/weather-dashboard"
+                    >Weather Dashboard</a
+                    >
+
+                </p>
             </div>
 
         </div>
+
     )
 }
 
